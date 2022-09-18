a65ae35e2fb08db41a8040d4f53c6d1e83f3cfd2
Anton Manuylov <killsonic4@gmail.com>
2022-09-19 00:21:20 +0300

    Add project files.

---
+++ Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        public static int NumMoreZero(int num)
+        {
+            int count = 0;
+            int temp;
+            for (int i = 1; i < num+1; i++)
+            {
+                Console.WriteLine($"Введите {i}е число");
+                temp = Convert.ToInt32((Console.ReadLine()));
+                if (temp > 0) { count++; }
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество цифр: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Количество чисел больше нуля: " + NumMoreZero(num));
+        }
+    }
+}
